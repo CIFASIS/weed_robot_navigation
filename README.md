30f7d0ab74449946fe3fe042ff6080047caa2fb8
Ismael Ait <ismaelaitd@gmail.com>
2021-01-25 00:30:25 -0300

    Add script and update README

--- README.md
+++ README.md
@@ -2,3 +2,27 @@
 
 Navegación del robot desmalezador.
 
+El script [bin/weed_robot.sh](bin/weed_robot.sh) se utiliza para levantar todos los componentes necesarios para la simulación del robot desmalezador y la ejecución de la navegación. Recibe los siguientes parámetros
+* -w, --world WORLD_NAME: nombre del archivo SDF que describe el escenario de simulación en Gazebo ubicado en *weed_robot_gazebo/worlds*.
+* -m, --map MAP_NAME: nombre de la imagen PGM con el mapa del entorno ubicado en *weed_robot_navigation/maps*.
+* -p, --pose POSE: posición y orientación donde se ubicará al robot al inicio, ejemplo *"-x -4.16 -y 1.0 -Y 1.5708"*.
+* -g, --gui: si se desea levantar la interfaz visual de Gazebo.
+* -v, --visualization: si se desea levantar el visualizador RViz.
+* -s, --sensor: si se ejecuta la generación de nube de puntos para el mapa local, caso contrario se usa el mapa estático.
+* -n, --navigation: si se quiere ejecutar la navegación, caso contrario solo se muestra el robot.
+* -t, --tracks TRACK_SEQUENCE: secuencia de franjas a recorrer, ejemplo *"[2,4,1,3]"*.
+* -l, --last POSITION: posición en y de la generación del último waypoint.
+* -p, --omega: para generar giros del tipo Omega en lugar de T.
+* -b, --bottom: si el robot inicia la primer franja desde abajo, caso contrario la iniciará desde arriba.
+
+Ejemplos:
+
+```
+$ ./weed_robot.sh -w obstacles -m obstacles -p "-x 0.0 -y 0.0 -Y 0.0" -g -v
+```
+
+```
+$ ./weed_robot.sh -w field -m field -p "-x -14.56 -y 1.0 -Y 1.5708" -g -v -s -n -t "[3, 4]" -l "-10.0"
+```
+
+
---
+++ bin/weed_robot.sh
@@ -0,0 +1,136 @@
+#!/bin/bash
+
+#./field_test.sh -n -t "[3, 4]" -v -p "-x -14.56 -y 1.0 -Y 1.570796327" -l "-10.0"
+#./field_test.sh -n -t "[3, 8]" -v -p "-x -14.56 -y 1.0 -Y 1.570796327" -l "-10.0"
+
+#./field_test.sh -n -t "[3, 4]" -v -p "-x -14.56 -y -10.5 -Y 1.570796327"
+#./field_test.sh -n -t "[4, 3]" -v -p "-x -12.48 -y -10.5 -Y 1.570796327"
+#./field_test.sh -n -t "[17, 14]" -v -p "-x 14.56 -y -10.5 -Y 1.570796327"
+#./field_test.sh -n -t "[17, 14]" -v -b -p "-x 14.56 -y 10.5 -Y -1.570796327"
+#./field_test.sh -n -t "[8, 5]" -v -p "-x -4.16 -y -10.5 -Y 1.570796327"
+#./field_test.sh -n -t "[11, 8]" -v -b -p "-x 2.08 -y -1 -Y -1.570796327"
+#./field_test.sh -n -t "[15, 11]" -v -b -p "-x 10.4 -y -1 -Y -1.570796327"
+
+#./field_test.sh -n -t "[8, 9]" -g -v -p "-x -4.16 -y 1.0 -Y 1.570796327" -l "-10.0"
+
+# defaults
+world="empty"
+map="field"
+navigation=false
+pose="-x -14.56 -y -11.0 -Y 1.570796327"
+gui=false
+visualization=false
+tracks="[1, 2]"
+omega=false
+bottom=false
+last=4.0
+sensor=false
+
+POSITIONAL=()
+while [[ $# -gt 0 ]]
+do
+key="$1"
+
+case $key in
+    -w|--world)
+    world="$2"
+    shift # past argument
+    shift # past value
+    ;;
+    -m|--map)
+    map="$2"
+    shift # past argument
+    shift # past value
+    ;;
+    -n|--navigation)
+    navigation=true
+    shift # past argument
+    ;;
+    -s|--sensor)
+    sensor=true
+    shift # past argument
+    ;;
+    -p|--pose)
+    pose="$2"
+    shift # past argument
+    shift # past value
+    ;;
+    -l|--last)
+    last="$2"
+    shift # past argument
+    shift # past value
+    ;;
+    -g|--gui)
+    gui=true
+    shift # past argument
+    ;;
+    -t|--tracks)
+    tracks="$2"
+    shift # past argument
+    shift # past value
+    ;;
+    -v|--visualization)
+    visualization=true
+    shift # past argument
+    ;;
+    -o|--omega)
+    omega=true
+    shift # past argument
+    ;;
+    -b|--bottom)
+    bottom=true
+    shift # past argument
+    ;;
+    *)    # unknown option
+    POSITIONAL+=("$1") # save it in an array for later
+    shift # past argument
+    ;;
+esac
+done
+set -- "${POSITIONAL[@]}" # restore positional parameters
+
+echo "Ejecutando simulación del robot desmalezador"
+echo "World Gazebo: ${world}"
+echo "Mapa global: ${map}"
+echo "Navigation: ${navigation}"
+echo "Pose: ${pose}"
+echo "Gui: ${gui}"
+
+sleep 1s
+roslaunch weed_robot_gazebo gazebo.launch world:="${world}" pose:="${pose}" gui:="${gui}" &
+
+if [ "${sensor}" = true ];
+then
+  local_costmap="local_costmap_params"
+else
+  local_costmap="local_costmap_static_params"
+fi
+
+sleep 4s
+roslaunch weed_robot_navigation navigation.launch global_map:="${map}" visualization:="${visualization}" \
+  rviz:=teb local_costmap:="${local_costmap}" &
+
+sleep 2s
+if [ "${sensor}" = true ];
+then
+  echo "Ejecutando generación de nube de puntos"
+  roslaunch weed_robot_navigation sensor.launch common:=navigation_common_params &
+else
+  echo "No se ejecutará la generación de nube de puntos"
+fi
+
+sleep 2s
+if [ "${navigation}" = true ];
+then
+  echo "Ejecutando navegación"
+  roslaunch weed_robot_navigation waypoint.launch common:=navigation_common_params \
+    travel_last:=true tracks:="${tracks}" use_omega:="${omega}" bottom:="${bottom}" last_offset:="${last}"
+else
+  echo "No se ejecutará navegación"
+fi
+
+wait
+
+# ps -aux | less | grep gazebo
+# kill pid
+
